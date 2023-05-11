531cfd43f91fe826f82b739d55c5e5da6ab3a16d
daniloramin <daniloramin00@hotmail.com>
2023-05-11 20:11:12 -0300

    create content to error page and style

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,5 +1,15 @@
 import "./style.scss";
 
+import { Link } from "react-router-dom";
+
 export const Error = () => {
-  return <div>Error page</div>;
+  return (
+    <div id="error-page">
+      <header>Ops, ocorreu um erro!</header>
+
+      <p>Essa página ainda não existe.</p>
+
+      <Link to="/">Voltar para Homepage</Link>
+    </div>
+  );
 };
